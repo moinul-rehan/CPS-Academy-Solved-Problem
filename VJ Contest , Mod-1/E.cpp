9a58a343345b56c3d7b7c8f61ba9003929153906
moinul-rehan <mir281310@gmail.com>
2023-07-08 21:00:33 +0600

    e cpp file update

--- VJ Contest , Mod-1/E.cpp
+++ VJ Contest , Mod-1/E.cpp
@@ -1,1 +1,15 @@
+#include<bits/stdc++.h>
+#include <iomanip>
 
+int main() {
+    int a, b;
+    std::cin >> a >> b;
+
+    int d = a / b;                 // Integer division
+    int r = a % b;                 // Remainder
+    double f = static_cast<double>(a) / b;  // Real number division
+
+    std::cout << d << " " << r << " " << std::fixed << std::setprecision(5) << f << std::endl;
+
+    return 0;
+}
